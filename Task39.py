34d91d822e4203d7fd22f0a86cd609229e8638f9
Masha <suvorova-2007@mail.ru>
2023-04-17 14:02:44 +0300

    Task39 solution

--- Task39.py
+++ Task39.py
@@ -10,3 +10,17 @@
 6
 4 15 43 1 15 1
  Вывод: 3 3 2 12"""
+
+ n = int(input())
+numbers1 = input().split()
+numbers1 = [int(numbers1[i]) for i in range(n)]
+
+m = int(input())
+numbers2 = input().split()
+numbers2 = [int(numbers2[i]) for i in range(m)]
+
+dif_num = list()
+for i in numbers1:
+    if i not in numbers2:
+        dif_num.append(i)
+print(*dif_num)
