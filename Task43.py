b6477f06682a4c1c1b2c4b6f57b772b21983c0b5
Masha <suvorova-2007@mail.ru>
2023-04-25 14:21:50 +0300

    Task43 solution

--- Task43.py
+++ Task43.py
@@ -6,3 +6,17 @@
 строках.
 Ввод:        Вывод:
 1 2 3 2 3    2"""
+
+numbers = input().split()
+numbers = [int(numbers[i]) for i in range(len(numbers))]
+count = 0
+while numbers:
+    i = numbers[0]
+    if numbers.count(i) >= 2:
+        count += int(numbers.count(i)//2)
+        while i in numbers:
+            numbers.remove(i)
+    else:
+        numbers.remove(i)
+print(count)
+    
